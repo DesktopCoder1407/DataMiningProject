cf69e61566b97a23462d34e0671efbf9622953b2
DesktopCoder1407 <dcoder1407@gmail.com>
2022-11-01 17:29:57 -0400

    Complete visualization portion. Save all figure to files.

--- visualization.py
+++ visualization.py
@@ -4,32 +4,20 @@ import pandas
 
 data = pandas.read_csv('data/cleaned_bank_churners.csv')
 
-data_dict = {'Customer_Age': data['Customer_Age'], 'Dependent_count': data['Dependent_count'],
-             'Months_on_book': data['Months_on_book'], 'Total_Relationship_Count': data['Total_Relationship_Count'],
-             'Months_Inactive_12_mon': data['Months_Inactive_12_mon'], 'Contacts_Count_12_mon': data['Contacts_Count_12_mon'],
-             'Credit_Limit': data['Credit_Limit'], 'Total_Revolving_Bal': data['Total_Revolving_Bal'],
-             'Avg_Open_To_Buy': data['Avg_Open_To_Buy'], 'Total_Trans_Amt': data['Total_Trans_Amt'],
-             'Total_Trans_Ct': data['Total_Trans_Ct']}
 
-# Years: Customer_Age
-# Small_Numeric: Dependent_count, Total_Relationship_Count, Contacts_Count_12_mon
-# Months: Months_on_book, Months_Inactive_12_mon
-# Money: Credit_Limit, Avg_Open_To_Buy
-# Balance: Total_Revolving_Bal, Total_Trans_Amt
-# Transaction Count: Total_Trans_Ct
+def plot_boxplots():
+    plotted_data = [('Customer_Age', 'Years'), ('Dependent_count', ''), ('Total_Relationship_Count', ''),
+                    ('Contacts_Count_12_mon', ''), ('Months_on_book', 'Months'), ('Months_Inactive_12_mon', 'Months'),
+                    ('Credit_Limit', 'Dollars'), ('Avg_Open_To_Buy', 'Dollars'), ('Total_Revolving_Bal', 'Dollars'),
+                    ('Total_Trans_Amt', 'Dollars'), ('Total_Trans_Ct', '')]
 
-# Years
-plt.boxplot(data['Customer_Age'])
-plt.xticks([1], ['Customer_Age'])
-plt.ylabel('Years')
-plt.show()
+    for item in plotted_data:
+        plt.boxplot(data[item[0]])
+        plt.xticks([1], [item[0]])
+        plt.ylabel(item[1])
+        #plt.show()
+        plt.savefig('visualization/' + item[0] + '.png')
 
-# Small_Numeric
-fig, ax = plt.subplots()
-ax.boxplot([data['Dependent_count', 'Total_Relationship_Count', 'Contacts_Count_12_mon']])
-ax.set_xticklabels(['Dependent_count', 'Total_Relationship_Count', 'Contacts_Count_12_mon'])
-plt.ylabel('')
-plt.show()
 
 def plot_hishiryo():
     hishiryo_converter = Hishiryo.Hishiryo()
@@ -38,3 +26,6 @@ def plot_hishiryo():
     hishiryo_converter.convertCSVToRadialBitmap('data/cleaned_bank_churners.csv', ',', output_path, radius, ['Customer_Age', 'Dependent_count', 'Months_on_book', 'Total_Relationship_Count',
                             'Months_Inactive_12_mon', 'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
                             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1'], 'Polygon')
+
+
+plot_boxplots()
