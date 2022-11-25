d1086f81a4fc1ea5c1749783b0d3ac1a3cb8ce59
shpetimberdyna <116901524+shpetimberdyna@users.noreply.github.com>
2022-11-25 17:22:57 -0500

    visualization

--- k-nearest_neighbors.py
+++ k-nearest_neighbors.py
@@ -1,6 +1,8 @@
 from preprocessing import build_PCA
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Loading data
@@ -17,10 +19,24 @@ y = data.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size = 0.2, random_state=42)
 
-knn = KNeighborsClassifier(n_neighbors=7)
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
 
-knn.fit(X_train, y_train)
+# Loop over K values
+for i, k in enumerate(neighbors):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
 
+    # Compute training and test data accuracy
+    train_accuracy[i] = knn.score(X_train, y_train)
+    test_accuracy[i] = knn.score(X_test, y_test)
 
-print(knn.score(X_test, y_test))
+# Generate plot
+plt.plot(neighbors, test_accuracy, label = 'Testing dataset Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training dataset Accuracy')
 
+plt.legend()
+plt.xlabel('n_neighbors')
+plt.ylabel('Accuracy')
+plt.show()
