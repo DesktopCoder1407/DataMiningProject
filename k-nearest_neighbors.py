b71e49a3bc40fbcefde020997a6f8792c47d1dd7
Nuyube <81436002+Nuyube@users.noreply.github.com>
2022-11-27 16:48:39 -0500

    Fix k-nearest neighbors and generate visualization up to k=19

--- k-nearest_neighbors.py
+++ k-nearest_neighbors.py
@@ -12,14 +12,14 @@ x = data.drop(columns=['Attrition_Flag'])
 y = data['Attrition_Flag']
 
 # Create feature and target arrays
-X = data.data
-y = data.target
+#X = data.values
+#y = data.target
 
 # Split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size = 0.2, random_state=42)
+    x, y, test_size = 0.2, random_state=42)
 
-neighbors = np.arange(1, 9)
+neighbors = np.arange(1, 19)
 train_accuracy = np.empty(len(neighbors))
 test_accuracy = np.empty(len(neighbors))
 
